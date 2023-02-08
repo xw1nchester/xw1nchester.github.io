39e90998b8af9c7092c15741a8da951f616ffd28
vetrovegor <71012752+vetrovegor@users.noreply.github.com>
2023-02-09 00:07:24 +0500

    Add files via upload

--- orionperfumery/js/script.js
+++ orionperfumery/js/script.js
@@ -27,9 +27,7 @@ let isMobile = {
 // изменение ширины экрана
 window.addEventListener('resize', function (e) {
     if (window.innerWidth > 767) {
-        burgerBtn.classList.remove('active');
-        menuPopup.classList.remove('active');
-        body.classList.remove('lock');
+        closeMenu();
     }
 
     if (window.innerWidth > 576) {
@@ -38,17 +36,26 @@ window.addEventListener('resize', function (e) {
 });
 
 // бургер меню
-let burgerBtn = document.querySelector('.burger-btn');
+let openMenuBtn = document.querySelector('.burger-btn');
 let menuPopup = document.querySelector('.menu-popup');
 let body = document.body;
 
-burgerBtn.addEventListener('click', function (e) {
+openMenuBtn.addEventListener('click', function (e) {
     e.preventDefault();
-
-    burgerBtn.classList.toggle('active');
     menuPopup.classList.toggle('active');
     body.classList.toggle('lock');
 })
+
+let closeMenuBtn = document.querySelector('.close-btn');
+closeMenuBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    closeMenu();
+});
+
+function closeMenu() {
+    menuPopup.classList.remove('active');
+    body.classList.remove('lock');
+}
 
 // картинка в блоке about-us
 let image = document.querySelector('.about-us__image');
@@ -75,5 +82,91 @@ function initImageHeight() {
 }
 
 document.addEventListener('onload', initImageHeight());
-document.addEventListener('DOMNodeInserted', initImageHeight());
-document.addEventListener('DOMNodeRemoved', initImageHeight());
+
+//slider
+new Swiper('.slider-slider-block', {
+    navigation: {
+        nextEl: '.slider-slider-block__btn_next',
+        prevEl: '.slider-slider-block__btn_prev',
+    },
+    pagination: {
+        el: '.slider-slider-block__pagination',
+        clickable: true,
+    },
+
+    loop: true,
+
+    breakpoints: {
+        767: {
+
+        },
+    }
+});
+
+// переход к рвзделам страницы
+const menuLinks = document.querySelectorAll('.header__nav a[data-goto]');
+
+menuLinks.forEach(l => l.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    let menuLink = e.target;
+
+    if (menuLink.dataset.goto && document.querySelector(`.${menuLink.dataset.goto}`)) {
+        let gotoBlock = document.querySelector(`.${menuLink.dataset.goto}`);
+        let gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset
+
+        window.scrollTo({
+            behavior: "smooth",
+            top: gotoBlockValue,
+        });
+
+        if (menuPopup.classList.contains('active')) {
+            closeMenu();
+        }
+    }
+}));
+
+// попап формы
+let form = document.querySelector('.contact__form').cloneNode(1);
+let formPopupInner = document.querySelector('.form-popup__inner');
+formPopupInner.appendChild(form);
+
+let openFormPopupBtn = document.querySelector('.header__btn');
+let formPopup = document.querySelector('.form-popup');
+
+openFormPopupBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    formPopup.classList.add('active');
+    body.classList.add('lock');
+});
+
+document.addEventListener('click', function (e) {
+    if (!e.target.closest('.header__btn')
+        && !e.target.closest('.form-popup__inner')
+        && formPopup.classList.contains('active')
+        || e.target.closest('.form-popup__close-btn')) {
+        formPopup.classList.remove('active');
+        body.classList.remove('lock');
+    }
+});
+
+// карта
+let center = [25.155679, 55.300266];
+
+function initMap() {
+    let map = new ymaps.Map('map', {
+        center: center,
+        zoom: 14
+    });
+
+    map.controls.remove('geolocationControl'); // удаляем геолокацию
+    map.controls.remove('searchControl'); // удаляем поиск
+    map.controls.remove('trafficControl'); // удаляем контроль трафика
+    map.controls.remove('typeSelector'); // удаляем тип
+    map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+    map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+    map.controls.remove('rulerControl'); // удаляем контрол правил
+    // map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+}
+
+ymaps.ready(initMap);
