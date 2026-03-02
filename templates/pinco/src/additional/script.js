c3f5f263bb39e285943c8fc091e6bcb8bf32c476
xw1nchester <vetroveg.72@mail.ru>
2026-03-02 14:29:48 +0500

    upd

--- templates/pinco/src/additional/script.js
+++ templates/pinco/src/additional/script.js
@@ -8,17 +8,39 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    const swiperCategoryTabs = new Swiper('.pu-home-games-widget-category-tabs', {
-        slidesPerView: 'auto',
-        freeMode: true
-    });
+    const swiperCategoryTabs = new Swiper(
+        '.pu-home-games-widget-category-tabs',
+        {
+            slidesPerView: 'auto',
+            freeMode: true
+        }
+    );
 
     const tournamentSlider = new Swiper('.ui-promo-tournaments-slider', {
         slidesPerView: 'auto',
         spaceBetween: 16,
         navigation: {
-        nextEl: '.ui-promo-tournaments-slider__control-next',
-        prevEl: '.ui-promo-tournaments-slider__control-prev',
-    }
+            nextEl: '.ui-promo-tournaments-slider__control-next',
+            prevEl: '.ui-promo-tournaments-slider__control-prev'
+        }
     });
-})
+
+    const sidebar = document.querySelector('.pu-sidebar');
+    const sidebarClose = document.querySelector('.menu-blose-btn');
+
+    document.addEventListener('click', function (e) {
+        const targetEl = e.target;
+
+        if (targetEl.closest('.menu-btn')) {
+            document.body.classList.add('lock');
+            sidebar.classList.add('active');
+            sidebarClose.classList.add('active');
+        }
+
+        if (targetEl.closest('.menu-blose-btn')) {
+            document.body.classList.remove('lock');
+            sidebar.classList.remove('active');
+            sidebarClose.classList.remove('active');
+        }
+    });
+});
