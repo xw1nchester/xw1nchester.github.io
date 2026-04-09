f023440aba508d57651abec970c309c47c22860d
xw1nchester <vetroveg.72@mail.ru>
2026-04-09 13:42:01 +0500

    upd

---
+++ templates/cat/src/additional/script.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mainSlider = document.querySelector('.main-slider');
+
+    new Swiper('.main-slider', {
+        navigation: {
+            nextEl: '.wlc-swiper-button-next',
+            prevEl: '.wlc-swiper-button-prev'
+        },
+        // pagination: {
+        //     el: '.selector',
+        //     clickable: true,
+        // },
+
+        slidesPerView: 1,
+        loop: true,
+        autoplay: {
+            delay: 5000
+        },
+
+        breakpoints: {
+            768: {}
+        }
+    });
+
+    new Swiper('.categories-slider', {
+        navigation: {
+            nextEl: '.wlc-category-menu__control--next',
+            prevEl: '.wlc-category-menu__control--prev'
+        },
+
+        slidesPerView: 'auto',
+
+        breakpoints: {
+            768: {}
+        }
+    });
+
+    const body = document.body;
+    const sidebar = document.querySelector('.sidebar');
+
+    document.addEventListener('click', function (e) {
+        const targetEl = e.target;
+
+        if (targetEl.closest('.burger-btn')) {
+            body.classList.add('lock');
+            sidebar.classList.add('wlc-float-panels--open');
+        }
+
+        if (
+            (targetEl.closest('.sidebar') &&
+                !targetEl.closest('.wlc-burger-panel')) ||
+            targetEl.closest('.wlc-burger-panel__close')
+        ) {
+            body.classList.remove('lock');
+            sidebar.classList.remove('wlc-float-panels--open');
+        }
+    });
+});
